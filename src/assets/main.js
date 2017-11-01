3cd21f68a89adc21091032a9ab7d765e884c47d7
bogdan <bogdan.bordeianu@gmail.com>
2017-11-01 15:55:40 +0100

    Make an Ajax Call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/1924423.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
 
 });
